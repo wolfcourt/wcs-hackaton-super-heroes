acb177638fb225dd70c9df132166a94e88eb33b2
nooneexpectme <erwann0danguy@gmail.com>
2018-04-05 22:07:33 +0200

    Update PHPDoc of the API

--- inc/SuperHeroes/Api.php
+++ inc/SuperHeroes/Api.php
@@ -24,6 +24,11 @@ class Api {
         return $this->httpClient;
     }
 
+    /**
+     * Return a json parsed object of a request
+     * @param string $path
+     * @return array
+     */
     private function getJSON(string $path): array
     {
         try {
@@ -34,6 +39,10 @@ class Api {
         }
     }
 
+    /**
+     * Return list of all characters
+     * @return Character[]
+     */
     public function getAllCharacters(): array
     {
         $data = $this->getJSON('all.json');
@@ -43,36 +52,66 @@ class Api {
         return $characters;
     }
 
+    /**
+     * Return a specific character
+     * @param int $characterId
+     * @return Character
+     */
     public function getCharacter(int $characterId): Character
     {
         $data = $this->getJSON("id/$characterId.json");
         return new Character($data);
     }
 
+    /**
+     * Return stats of a character
+     * @param int $characterId
+     * @return PowerStats
+     */
     public function getPowerStats(int $characterId): PowerStats
     {
         $data = $this->getJSON("powerstats/$characterId.json");
         return new PowerStats($data);
     }
 
+    /**
+     * Return appearance class of a character
+     * @param int $characterId
+     * @return Appearance
+     */
     public function getAppearance(int $characterId): Appearance
     {
         $data = $this->getJSON("appearance/$characterId.json");
         return new Appearance($data);
     }
 
+    /**
+     * Return the biography of a character
+     * @param int $characterId
+     * @return Biography
+     */
     public function getBiography(int $characterId): Biography
     {
         $data = $this->getJSON("biography/$characterId.json");
         return new Biography($data);
     }
 
+    /**
+     * Return connections of a character
+     * @param int $characterId
+     * @return Connections
+     */
     public function getConnections(int $characterId): Connections
     {
         $data = $this->getJSON("connections/$characterId.json");
         return new Connections($data);
     }
 
+    /**
+     * Return work of a character
+     * @param int $characterId
+     * @return Work
+     */
     public function getWork(int $characterId): Work
     {
         $data = $this->getJSON("work/$characterId.json");
