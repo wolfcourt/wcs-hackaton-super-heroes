87c1b55093288bc97f1688572f858ee73e11638a
nooneexpectme <erwann0danguy@gmail.com>
2018-04-06 05:19:36 +0200

    Add caching on data/selection

--- data/selection.php
+++ data/selection.php
@@ -1,11 +1,18 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     require '../vendor/autoload.php';
 
     use App\SuperHeroes\Api;
 
+    $cacheFile = __DIR__ . '/cache/selection.json';
+    if (file_exists($cacheFile))
+        die(file_get_contents($cacheFile));
+
     $api = new Api();
+
     $characters = $api->getAllCharacters();
+    shuffle($characters);
     $dataToSend = [];
 
     foreach ($characters AS $character) {
@@ -15,7 +22,8 @@
             'slug' => $character->getSlug(),
             'image' => $character->getImages()->getLg(),
         ];
-        if(count($dataToSend) === 15) break;
+        if(count($dataToSend) === 30) break;
     }
 
+    file_put_contents($cacheFile, json_encode($dataToSend));
     echo json_encode($dataToSend);
